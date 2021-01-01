02ebce046df81eb24b61abc9025a1df8b1c1bd5c
ZijiaqiXu <73362637+ZijiaqiXu@users.noreply.github.com>
2021-01-01 09:17:59 -0500

    Add files via upload

---
+++ dht-streaming/dht-streaming/src/edu/stevens/cs549/dhts/resource/NodeResource.java
@@ -0,0 +1,129 @@
+package edu.stevens.cs549.dhts.resource;
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.glassfish.jersey.media.sse.SseFeature;
+
+
+@Path("/dht")
+public class NodeResource {
+
+	/*
+	 * Web service API.
+	 * 
+	 * Fill in the missing operations.
+	 */
+
+	Logger log = Logger.getLogger(NodeResource.class.getCanonicalName());
+
+	@Context
+	UriInfo uriInfo;
+
+	@Context
+	HttpHeaders headers;
+	
+	@GET
+	@Path("info")
+	@Produces("application/xml")
+	public Response getNodeInfoXML() {
+		return new NodeService(headers, uriInfo).getNodeInfo();
+	}
+
+	@GET
+	@Path("info")
+	@Produces("application/json")
+	public Response getNodeInfoJSON() {
+		return new NodeService(headers, uriInfo).getNodeInfo();
+	}
+
+	@GET
+	@Produces("application/xml")
+	public Response getBinding(@QueryParam("key") String k) {
+		return new NodeService(headers, uriInfo).getBinding(k);
+	}
+
+	@PUT
+	public Response putBinding(@QueryParam("key") String k,@QueryParam("val") String v) {
+		return new NodeService(headers, uriInfo).addBinding(k, v);
+		// NodeInfo p = predDb.getInfo();
+	}
+	
+	@DELETE
+	public Response deleteBinding(@QueryParam("key") String k,@QueryParam("val") String v) {
+		return new NodeService(headers, uriInfo).deleteBinding(k, v);
+	}
+	
+	@GET
+	@Path("succ")
+	@Produces("application/xml")
+	public Response getSucc() {
+		return new NodeService(headers, uriInfo).getSucc();
+	}
+	
+	@GET
+	@Path("pred")
+	@Produces("application/xml")
+	public Response getPred() {
+		return new NodeService(headers, uriInfo).getPred();
+	}
+	
+	@GET
+	@Path("finger")
+	@Produces("application/xml")
+	public Response getFinger(@QueryParam("id") String index) {
+		int id = Integer.parseInt(index);
+		return new NodeService(headers, uriInfo).closestPrecedingFinger(id);
+	}
+
+	@PUT
+	@Path("notify")
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	/*
+	 * Actually returns a TableRep (annotated with @XmlRootElement)
+	 */
+	public Response putNotify(TableRep predDb) {
+		/*
+		 * See the comment for WebClient::notify (the client side of this logic).
+		 */
+		return new NodeService(headers, uriInfo).notify(predDb);
+		// NodeInfo p = predDb.getInfo();
+	}
+	
+	@GET
+	@Path("find")
+	@Produces("application/xml")
+	public Response findSuccessor(@QueryParam("id") String index) {
+		int id = Integer.parseInt(index);
+		return new NodeService(headers, uriInfo).findSuccessor(id);
+	}
+
+	// SSE methods returns output stream
+	@GET
+	@Path("listen")
+	@Produces(SseFeature.SERVER_SENT_EVENTS)
+	public Response getOutputStream(@QueryParam("id") String index, @QueryParam("key") String key) {
+		int id = Integer.parseInt(index);
+		return new NodeService(headers, uriInfo).listenForBindings(id, key);
+	}
+
+	// delete SSE listener
+	@DELETE
+	@Path("listen")
+	public Response stopListening(@QueryParam("id") String index, @QueryParam("key") String key) {
+		int id = Integer.parseInt(index);
+		return new NodeService(headers, uriInfo).stopListening(id, key);
+	}
+}
